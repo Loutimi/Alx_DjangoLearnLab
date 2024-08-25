9b029e815dde28cbae191930c91f68c8e3690d8e
Loutimi <Loutimi59@gmail.com>
2024-08-25 19:38:12 +0100

    Modify relationship_app/admin_view.py

--- django-models/relationship_app/admin_view.py
+++ django-models/relationship_app/admin_view.py
@@ -1,9 +1,10 @@
-from django.contrib import admin
-from .models import UserProfile
-# Register your models here.
+from django.contrib.auth.decorators import user_passes_test
+from django.shortcuts import render
+from django.http import HttpResponseForbidden
 
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
 
-@admin.register(UserProfile)
-class UserProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'role')
-    search_fields = ('user__username',)
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'admin_template.html')
