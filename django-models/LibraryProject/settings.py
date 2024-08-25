1254c9cca818329ecbfa5d312d3e55876f91544c
Loutimi <Loutimi59@gmail.com>
2024-08-25 19:56:07 +0100

    Modify LibraryProject/settings.py

--- django-models/LibraryProject/settings.py
+++ django-models/LibraryProject/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-import os
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bookshelf',
-    'relationship_app.apps.RelationshipAppConfig'
+    'relationship_app',
 ]
 
 MIDDLEWARE = [
