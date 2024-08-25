1bac88aead8f6799a9001d44c801ddd17d680c7d
Loutimi <Loutimi59@gmail.com>
2024-08-25 12:40:01 +0100

    Modify django-models/relationship_app/views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,3 +1,56 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Book
+from .models import Library
+from django.db import models
+from django.views.generic.detail import DetailView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate
+from django.contrib.auth import login
+from django.contrib.auth import logout
+from django.contrib import messages
+from django.contrib.auth.models import User
 
 # Create your views here.
+
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'relationship_app/list_books.html', {'books': books})
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+
+def LoginView(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')  # Redirect to a success page.
+        else:
+            messages.error(request, 'Invalid username or password.')
+    return render(request, 'relationship_app/login.html')
+
+def LogoutView(request):
+    logout(request)
+    return redirect('login')  
+
+def homeView(request):
+    return render(request, 'relationship_app/home.html')
+
+
+def registerView(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Log the user in after successful registration
+            return redirect('home')
+        else:
+            for error in form.errors.values():
+                messages.error(request, error)
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
