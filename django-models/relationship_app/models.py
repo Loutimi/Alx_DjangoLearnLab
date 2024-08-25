d643b1e476eec9063534feb2e420c726674a6d0a
Loutimi <Loutimi59@gmail.com>
2024-08-25 19:50:17 +0100

    Modify relationship_app/models.py

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -1,8 +1,7 @@
 from django.db import models
-from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login
-from django.contrib.auth.decorators import login_required
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 class Author(models.Model):
@@ -14,32 +13,42 @@ class Author(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    publication_year = models.IntegerField(default=0)
+
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add a new book"),
+            ("can_change_book", "Can edit a book"),
+            ("can_delete_book", "Can delete a book"),
+        ]
 
     def __str__(self):
         return self.title
     
-
 class Library(models.Model):
     name = models.CharField(max_length=100)
     books = models.ManyToManyField(Book)
-
-    def __str__(self):
-        return self.name
 
 class Librarian(models.Model):
     name = models.CharField(max_length=100)
     library = models.OneToOneField(Library, on_delete=models.CASCADE)
 
-    def __str__(self):
-        return self.name
-    
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    password = models.CharField(max_length= 200)
-    def __str__(self):
-        return self.user.username
-    
-def create_or_update_user_profile(sender, instance, created, **kwargs):
+    role = models.CharField(max_length=60, choices=[
+        ('Admin', 'Admin'),
+        ('Librarian', 'Librarian'),
+        ('Member', 'Member')
+    ])
+
+    def __str__(self) -> str:
+        return f'{self.user.username}-{self.role}'
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
     if created:
-       UserProfile.objects.create(user=instance)
-    instance.profile.save()
+        UserProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user(sender, instance, created, **kwargs):
+    instance.userprofile.save()
