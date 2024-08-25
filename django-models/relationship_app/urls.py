01ddad8ced88191bf7dde42ea79d00c14f8f4d9e
Loutimi <Loutimi59@gmail.com>
2024-08-26 00:05:05 +0100

    Modify relationship_app/urls.py

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -11,7 +11,10 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(template_name='logout.html'), name='logout'),
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library-detailed-view'),
-    path('add_book/', views.add_book, name='add_book'),
-    path('books/<int:pk>/edit/', views.edit_book, name='edit_book'),
-    path('books/<int:pk>/delete/', views.delete_book, name='delete_book'),
+    path('admin/', views.admin_view, name='admin_view'),
+    path('librarian/', views.librarian_view, name='librarian_view'),
+    path('member/', views.member_view, name='member_view'),
+    path('add_book/', views.add_book, name='add_book'),  # Updated path for adding a book
+    path('edit_book/<int:book_id>/', views.edit_book, name='edit_book'),  # Updated path for editing a book
+    path('delete_book/<int:book_id>/', views.delete_book, name='delete_book'),  # Updated path for deleting a book
 ]
