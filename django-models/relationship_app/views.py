9e316545041f2785b8a589bac85fa50e1a87f877
Loutimi <Loutimi59@gmail.com>
2024-08-28 21:19:13 +0100

    Modify django-models/relationship_app/views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
-from django.views.generic.detail import DetailView
+from django.shortcuts import render, redirect
 from .models import Book 
 from .models import Library
 
+from django.views.generic.detail import DetailView
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 # Create your views here.
 
 def list_books(request):
@@ -16,3 +18,30 @@ class LibraryDetailView(DetailView):
   context_object_name = 'library'
   queryset = Library.objects.all()
 
+# User authentication features
+def register(request):
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid(): 
+      user = form.save()
+      login(request, user)
+      return redirect('home')  
+    
+  else:
+    form = UserCreationForm()
+  return render(request, 'relationship_app/register.html', {'form': form})
+
+def login_view(request):
+  if request.method == 'POST':
+    form = AuthenticationForm(request, data=request.POST)
+    if form.is_valid():
+      user = form.get_user()
+      login(request, user)
+      return redirect('home')  
+  else:
+    form = AuthenticationForm()
+  return render(request, 'relationship_app/login.html', {'form': form})
+
+def logout_view(request):
+  logout(request)
+  return redirect('home')
