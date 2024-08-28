b94d63af710df5cbbfdf647586e2d11b60e5af0c
Loutimi <Loutimi59@gmail.com>
2024-08-28 21:35:44 +0100

    Modify django-models/relationship_app/views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Book 
 from .models import Library
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.views.generic.detail import DetailView
 from django.contrib.auth import login, logout
 
