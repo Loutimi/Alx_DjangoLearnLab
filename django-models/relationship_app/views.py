131470cb94afdf1066d33d6844dde41bf9b422ad
Loutimi <Loutimi59@gmail.com>
2024-08-28 21:28:51 +0100

    Modify django-models/relationship_app/views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect
 from .models import Book 
 from .models import Library
-
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.views.generic.detail import DetailView
 from django.contrib.auth import login, logout
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+
 # Create your views here.
 
 def list_books(request):
