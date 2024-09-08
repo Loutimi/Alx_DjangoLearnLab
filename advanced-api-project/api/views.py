7bf249a596025cc6a63249bc79900a9d2e7d8e8f
Loutimi <loutimi59@gmail.com>
2024-09-08 20:59:28 +0100

    Modify advanced-api-project/api/views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Book
+from .serializers import BookSerializer
+from .filters import BookFilter
+
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
+
+from django_filters import rest_framework
+
+
+class BookListView(generics.ListAPIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    filter_backends = [filters.OrderingFilter, filters.SearchFilter]
+    ordering_fields = ['title', 'author', 'publication_year']
+    search_fields = ['title', 'publication_year']
+
+
+class BookDetailView(generics.RetrieveAPIView):
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookCreateView(generics.CreateAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookUpdateView(generics.UpdateAPIView):
+    permission_classess = [IsAuthenticated]
+
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
+class BookDeleteView(generics.DestroyAPIView):
+    permission_classes = [IsAuthenticated]
+
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
