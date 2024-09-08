80a6dcbbe686ae7cb88cbfd01fd563855f532553
Loutimi <loutimi59@gmail.com>
2024-09-08 21:27:13 +0100

    Modify advanced-api-project/api/views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,13 +2,15 @@ from rest_framework import generics
 
 from .models import Book
 from .serializers import BookSerializer
-from .filters import BookFilter
+
 
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
 
-from django_filters import rest_framework
+from django_filters import rest_framework as filters
+
+
 
 
 class BookListView(generics.ListAPIView):
