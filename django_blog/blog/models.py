8ed1facd6a2356038bce55d69b957d316a34ff55
Loutimi <loutimi59@gmail.com>
2024-09-11 13:26:39 +0100

    Modify django_blog/blog/models.py

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User 
+from taggit.managers import TaggableManager
+
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    tags = TaggableManager()
 
     def __str__(self):
         return self.title
@@ -23,3 +26,9 @@ class Comment(models.Model):
 
     def __str__(self):
         return f"{self.author.username} on {self.post.title}"
+    
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
