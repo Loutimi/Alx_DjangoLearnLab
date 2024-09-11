deff0f693df665b23be0c71a6166a8156ad1759a
Loutimi <loutimi59@gmail.com>
2024-09-11 12:39:17 +0100

    Modify django_blog/blog/forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserChangeForm
-
+from .models import Comment
 
 class CustomUserCreationForm(UserCreationForm):
     first_name = forms.CharField(max_length=200, required=True)
@@ -20,3 +20,7 @@ class CustomUserChangeForm(UserChangeForm):
         model = User
         fields = ('username', 'first_name', 'last_name', 'email')
         
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('content',)
