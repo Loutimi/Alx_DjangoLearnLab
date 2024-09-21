568bea3a2ca035d3f0561f8076deaaf0b457127c
Loutimi <Loutimi59@gmail.com>
2024-09-21 13:14:37 +0100

    Modify accounts/models.py

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -6,7 +6,9 @@ class CustomUser(AbstractUser):
     bio = models.TextField(null=True, blank=True)  
     profile_picture = models.ImageField(
         upload_to='profile_pictures/', null=True, blank=True)
+    # Explicitly set related_name for followers and following
     followers = models.ManyToManyField(
         'self', symmetrical=False, related_name='followed_by', blank=True)
     following = models.ManyToManyField(
         'self', symmetrical=False, related_name='follows', blank=True)
+
