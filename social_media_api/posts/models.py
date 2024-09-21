01148bdf7f03cafa43d2e3d415f2d5fd01176caf
Loutimi <Loutimi59@gmail.com>
2024-09-21 14:49:52 +0100

    Modify posts/models.py

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 from accounts.models import CustomUser
 
-
 class Post(models.Model):
     author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     title = models.CharField(max_length=250)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    likes = models.ManyToManyField(CustomUser, through='Like', related_name='liked_posts') # Added like functionality
+
 
     def __str__(self):
         return self.title
@@ -23,3 +24,13 @@ class Comment(models.Model):
     def __str__(self):
         return f'Comment by {self.author} on {self.post}'
 
+class Like(models.Model):
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('author','post')  # Prevent duplicate likes for a user on a post
+
+    def __str__(self):
+        return f'{self.post} liked by {self.author}'
