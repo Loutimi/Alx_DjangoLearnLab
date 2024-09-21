e80b48fb6e2dd0555c5f297a7ea3051eff1fec46
Loutimi <Loutimi59@gmail.com>
2024-09-21 14:08:28 +0100

    Modify accounts/views.py

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,17 +1,19 @@
-from django.shortcuts import render, get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.generics import CreateAPIView, RetrieveUpdateAPIView
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.permissions import IsAuthenticated, AllowAny
-from rest_framework.decorators import action
-from rest_framework.viewsets import ViewSet
+from rest_framework import permissions
+from rest_framework.permissions import AllowAny
+from rest_framework import generics
+# from rest_framework.decorators import action
+# from rest_framework.viewsets import ViewSet
 
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.authtoken.models import Token
 
-from django.contrib.auth import authenticate, get_user_model
-
+from django.contrib.auth import authenticate
+from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
 
 from .serializers import RegisterSerializer, UserSerializer, ProfileUpdateSerializer
 from .models import CustomUser
@@ -56,7 +58,7 @@ class LoginView(APIView):
 
 class ProfileView(RetrieveUpdateAPIView):
     authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
@@ -66,31 +68,44 @@ class ProfileView(RetrieveUpdateAPIView):
         return self.request.user
 
 
-class FollowViewSet(ViewSet):
+class FollowUserView(generics.GenericAPIView):
     authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
-    @action(detail=True, methods=['post'])
-    def follow_user(self, request, pk=None):
-        user_to_follow = get_object_or_404(CustomUser, pk=pk)
+    def post(self, request, user_id):
+        """Follow a user"""
+        user_to_follow = get_object_or_404(CustomUser, id=user_id)
+
+        if request.user == user_to_follow:
+            return Response({'detail': "You cannot follow yourself."}, status=status.HTTP_400_BAD_REQUEST)
         request.user.following.add(user_to_follow)
         return Response({'status': 'following'}, status=status.HTTP_200_OK)
 
-    @action(detail=True, methods=['post'])
-    def unfollow_user(self, request, pk=None):
-        user_to_unfollow = get_object_or_404(CustomUser, pk=pk)
+
+class UnfollowUserView(generics.GenericAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request, user_id):
+        """Unfollow a user"""
+        user_to_unfollow = get_object_or_404(CustomUser, id=user_id)
+
+        if request.user == user_to_unfollow:
+            return Response({'detail': "You cannot unfollow yourself."})
+
         request.user.following.remove(user_to_unfollow)
         return Response({'status': 'unfollowed'}, status=status.HTTP_200_OK)
 
 
 class FeedView(APIView):
     authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated]
 
     def get(self, request):
         user = request.user
-        followed_users = user.following.all()
+        following_users = user.following.all()
         posts = Post.objects.filter(
-            author__in=followed_users).order_by('-created_at')
+            author__in=following_users).order_by('-created_at')
         serialized_posts = PostSerializer(posts, many=True)
         return Response(serialized_posts.data)
+    
