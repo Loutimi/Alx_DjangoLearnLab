d5c836d144f84cd5007880c72fe869e0a31eb4c2
Loutimi <Loutimi59@gmail.com>
2024-09-21 15:18:52 +0100

    Modiify posts/views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,13 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-from rest_framework import viewsets, permissions, filters
+from rest_framework import viewsets, permissions, filters, generics, status
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
-from .models import Post, Comment
+from rest_framework.permissions import IsAuthenticated
+from .models import Post, Comment, Like
 from .serializers import PostSerializer, CommentSerializer
-
+from notifications.models import create_notification
 
 class PostViewSet(viewsets.ModelViewSet):
     authentication_classes = [TokenAuthentication]
@@ -41,3 +41,33 @@ class FeedView(APIView):
         posts = Post.objects.filter(author__in=following_users).order_by('-created_at')
         serialized_posts = PostSerializer(posts, many=True)
         return Response(serialized_posts.data)
+
+class LikePostView(generics.GenericAPIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
+
+        if Like.objects.filter(user=request.user, post=post).exists():
+            return Response({'error': 'You already liked this post'}, status=status.HTTP_400_BAD_REQUEST)
+
+        like = Like.objects.create(user=request.user, post=post)
+        serializer = PostSerializer(post)
+
+        if post.author != request.user:  # Don't notify yourself
+            create_notification(post.author, request.user, 'liked your post', post)
+
+        return Response(serializer.data)
+
+
+class UnlikePostView(generics.GenericAPIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        like = get_object_or_404(Like, user=request.user, post=post)
+
+        like.delete()
+        serializer = PostSerializer(post)
+
+        return Response(serializer.data)
