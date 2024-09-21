110c6912a59934be4d9a1027112f8cd55a0a4bff
Loutimi <Loutimi59@gmail.com>
2024-09-21 11:54:33 +0100

    Modify social_media_api/accounts/views.py

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,3 +1,96 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.generics import CreateAPIView, RetrieveUpdateAPIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.decorators import action
+from rest_framework.viewsets import ViewSet
 
-# Create your views here.
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.models import Token
+
+from django.contrib.auth import authenticate, get_user_model
+
+
+from .serializers import RegisterSerializer, UserSerializer, ProfileUpdateSerializer
+from .models import CustomUser
+
+# imports from posts app
+from posts.models import Post
+from posts.serializers import PostSerializer
+
+
+class RegisterView(CreateAPIView):
+    permission_classes = (AllowAny,)
+
+    queryset = CustomUser.objects.all()
+    serializer_class = RegisterSerializer
+
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class LoginView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        if username is None or password is None:
+            return Response({'error': 'Please provide both username and password'}, status=status.HTTP_400_BAD_REQUEST)
+
+        user = authenticate(username=username, password=password)
+
+        if not user:
+            return Response({'error': 'Invalid Credentials'}, status=status.HTTP_400_BAD_REQUEST)
+
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key})
+
+
+class ProfileView(RetrieveUpdateAPIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+    def get_object(self):
+        # Ensures a user can acces only own profile
+        return self.request.user
+
+
+class FollowViewSet(ViewSet):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    @action(detail=True, methods=['post'])
+    def follow_user(self, request, pk=None):
+        user_to_follow = get_object_or_404(CustomUser, pk=pk)
+        request.user.following.add(user_to_follow)
+        return Response({'status': 'following'}, status=status.HTTP_200_OK)
+
+    @action(detail=True, methods=['post'])
+    def unfollow_user(self, request, pk=None):
+        user_to_unfollow = get_object_or_404(CustomUser, pk=pk)
+        request.user.following.remove(user_to_unfollow)
+        return Response({'status': 'unfollowed'}, status=status.HTTP_200_OK)
+
+
+class FeedView(APIView):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        followed_users = user.following.all()
+        posts = Post.objects.filter(
+            author__in=followed_users).order_by('-created_at')
+        serialized_posts = PostSerializer(posts, many=True)
+        return Response(serialized_posts.data)
